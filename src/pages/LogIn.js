815262781907c727b1091ce6969af066a135899b
Ewan Mould <ewan.heath.mould@outlook.com>
2022-12-15 13:41:03 +0000

    ~Google log in added

--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import {supabase} from "../lib/supabaseClient";
-
-
 export default function LogIn() {
 
-
-    const logInGitHub = async () => {
+    const LogInGitHub = async () => {
       await supabase.auth.signInWithOAuth({
         provider: 'github'
       })
     }
-
+    const LogInGoogle = async  () => {
+      await  supabase.auth.signInWithOAuth({
+        provider: 'google'
+      })
+    }
     return (
       <>
         <h1>Login</h1>
-        <button onClick={logInGitHub}>Login with github</button>
+        <button onClick={LogInGitHub}>Login with github</button>
+        <button onClick={LogInGoogle}>Log in with Google</button>
+        <div>
+          <button>Don't have an account? Sign up here</button>
+        </div>
       </>
     );
 
